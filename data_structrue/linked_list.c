9088346dfaea4a93acb32221c571d7ad46d58d66
Ansh <63942964+Ansh-dev-2005@users.noreply.github.com>
2023-02-09 13:22:29 +0530

    vscode

--- data_structrue/linked_list.c
+++ data_structrue/linked_list.c
@@ -1,109 +1,65 @@
 //Demonstrate a program in C that will create and display Singly Linked List
 
-#include <stdio.h>
-#include <stdlib.h>
+#include<stdio.h>
+#include<stdlib.h>
 
 struct node
-{
-    int data;
-    struct node *next;
-};
+{ 
+    int num;
+    struct node *nextptr;
+}*stnode;
 
-struct node *head = NULL;
-struct node *current = NULL;
+void createNodelist(int n);
+void displaylist();
 
-//display the list
-void printList()
-{
-    struct node *ptr = head;
-    printf(" [head] =>");
-    //start from the beginning
-    while (ptr != NULL)
-    {
-        printf(" %d =>", ptr->data);
-        ptr = ptr->next;
-    }
-
-    printf(" [null] n");
+int main(){
+    int n;
+    printf("Enter the number of node \n");
+    scanf("%d",&n);
+    createNodelist(n);
+    printf("Data entered in the list \n");
+    displaylist();
+    return 0;
 }
 
-//insert link at the first location
-void insertFirst(int data)
-{
-    //create a link
-    struct node *link = (struct node*) malloc(sizeof(struct node));
-
-    link->data = data;
-    link->next = head;
-
-    head = link;
-}
-
-//delete first item
-struct node* deleteFirst()
-{
-
-    //save reference to first link
-    struct node *tempLink = head;
-
-    //mark next to first link as first
-    head = head->next;
-
-    //return the deleted link
-    return tempLink;
-}
-
-//is list empty
-
-int isEmpty()
-{
-    return head == NULL;
-}
-
-int length()
-{
-    int length = 0;
-    struct node *current;
-
-    for(current = head; current != NULL; current = current->next)
-    {
-        length++;
-    }
-
-    return length;
-}
-
-//find a link with given key
-struct node* find(int data)
-{
-
-    //start from the first link
-    struct node* current = head;
-
-    //if list is empty
-    if(head == NULL)
-    {
-        return NULL;
-    }
-
-    //navigate through list
-    while(current->data != data)
-    {
-
-        //if it is last node
-        if(current->next == NULL)
-        {
-            return NULL;
-        }
-        else
-        {
-            //go to next link
-            current = current->next;
+void createNodelist(int n){
+    struct node *fnNode, *tmp;
+    int num, i;
+    stnode = (struct node *)malloc(sizeof(struct node));
+    if(stnode == NULL){
+        printf("Memory can not be allocated");
+    }else{
+        printf("Enter the data for node 1: ");
+        scanf("%d",&num);
+        stnode->num = num;
+        stnode->nextptr = NULL;
+        tmp = stnode;
+        for(i=2;i<=n;i++){
+            fnNode = (struct node *)malloc(sizeof(struct node));
+            if(fnNode == NULL){
+                printf("Memory can not be allocated");
+                break;
+            }else{
+                printf("Enter the data for node %d: ",i);
+                scanf("%d",&num);
+                fnNode->num = num;
+                fnNode->nextptr = NULL;
+                tmp->nextptr = fnNode;
+                tmp = tmp->nextptr;
+            }
         }
     }
 
-    //if data found, return the current Link
-    return current;
 }
-
-//delete a link with given key
+void displaylist(){
+    struct node *tmp;
+    if(stnode == NULL){
+        printf("List is empty");
+    }else{
+        tmp = stnode;
+        while(tmp != NULL){
+            printf("Data = %d\n",tmp->num);
+            tmp = tmp->nextptr;
+        }
+    }
+}
